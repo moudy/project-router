a70c1c51d436e12d3b7c7c499b0fdcafa6a745dd
Moudy <moudy.elkammash@gmail.com>
2014-05-09 08:10:20 -0400

    basic functional tests

--- test/functional.js
+++ test/functional.js
@@ -1,4 +1,35 @@
+var path = require('path');
+var expect = require('chai').expect;
 var supertest = require('supertest');
 var app = require('express')();
 var projectRouter = require('../index');
 
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+var router = projectRouter.map(function () {
+  this.get('/users', 'users/index');
+  this.post('/users', 'users/index');
+  this.put('/users', 'users/index');
+  this.delete('/users', 'users/index');
+});
+
+app.use(router);
+
+describe('request/response', function () {
+  function test (verb, endpoint, expected) {
+    return function (done) {
+      supertest(app)[verb](endpoint).end(function (err, res) {
+        expect(res.text).to.contain(expected);
+        done();
+      });
+    };
+  }
+
+  it('renders users/index', test('get', '/users', 'users/index'));
+  it('renders users/index', test('post', '/users', 'users/index'));
+  it('renders users/index', test('put', '/users', 'users/index'));
+  it('renders users/index', test('del', '/users', 'users/index'));
+
+});
